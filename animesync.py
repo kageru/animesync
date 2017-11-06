42210f5f37e9706bffb96ed6fd94e051bd61fa5a
kageru <8497max@gmail.com>
2017-11-06 11:21:54 +0100

    lecture is over. already?

--- animesync.py
+++ animesync.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import pysftp
+import config
 import os
 
-def get_remote_filelist():
-    pass
+def get_remote_filelist(conn) -> list:
+    with conn.cd(config.remote_directory):
+        return conn.ls()
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,10 @@
+# host, username, and ssh key
+ftp_host = ''
+ftp_user = ''
+ftp_key = ''
+
+remote_directory = ''
+local_directory = ''
+
+remove_files_after_download=False
+
