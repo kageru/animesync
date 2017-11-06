17569cdcb1d60026120355cf082022272043b783
kageru <kageru@encode.moe>
2017-11-06 16:51:36 +0100

    connection works

--- animesync.py
+++ animesync.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
 import pysftp
-import config
+import config2
 import os
 
-def get_remote_filelist(conn) -> list:
-    with conn.cd(config.remote_directory):
-        return conn.ls()
+
+def get_remote_filelist(conn: pysftp.Connection) -> list:
+    return conn.listdir()
 
 
-if __name__ == '__main__':
+def local_file_exists(file):
     pass
 
+def download_file(file)
+
+if __name__ == '__main__':
+    with pysftp.Connection(host=config2.ftp_host, username=config2.ftp_user, private_key=config2.ftp_key) as conn:
+        with conn.cd(config2.remote_directory):
+            files = get_remote_filelist(conn)
+            for file in files:
+                if not local_file_exists(file):
+                    download_file()
